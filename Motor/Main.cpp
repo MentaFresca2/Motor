5bfe0fbb49f1be1fbc7096b927986a86f99ff6da
Rubén <gutierrezruben657@gmail.com>
2024-06-30 19:20:21 +0200

    Ahroa con camara y movimiento

--- Motor/Main.cpp
+++ Motor/Main.cpp
@@ -7,14 +7,26 @@
 #include "Shader.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-using namespace glm;
 
+
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
 int main()
 {
     //Definicion de funciones--------------------------------------------------------------
     void framebuffer_size_callback(GLFWwindow * window, int width, int height);
     void processInput(GLFWwindow * window);
-    void checkOpenGLError(const char* stmt, const char* fname, int line);
+    void mouse_callback(GLFWwindow * window, double xpos, double ypos);
     //Definicion de funciones--------------------------------------------------------------
 
     
@@ -57,6 +69,8 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); //Hace que capture el cursor y lo vuelve invisible
+    glfwSetCursorPosCallback(window, mouse_callback); //Da las coordenadas en pixeles del mouse y se las envia a mouse_callback para que las use
     glEnable(GL_DEPTH_TEST);//Hace que halla profundidad, esta se calcula cada frame
     glViewport(0, 0, 800, 600); //Define los pixeles en los que opengl va a renderizar cosas
 
@@ -273,6 +287,27 @@ int main()
 
 
 
+
+    //Como crear camara/como funciona:
+    /*
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f); //posicion de la camara
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f); //Lado contrario al que uqiero que la camara mire
+    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget); //se normalza (al normalizar)
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f); //Se crea un vector que mira para arriba
+    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection)); //se cruza con la direccion de la camara y se normaliza, el cruzado es para que pase de mirar para arriba al lado positivo de x
+    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight); //Para obtener arriba cruzamos otra vez esto, y como ya esta no rmalizado no hacemos nada mas
+    glm::mat4 view; //Crea la vista
+    view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 1.0f, 0.0f));
+        */
+    //lookat toma 3 argumentos
+    //Primero el vector de la posicion de la camara
+    //Luego a donde tiene que mirar la camara
+    //Luego un vector que mire hacia arriba (glm lo usa para crear un vector right)
+
+
+
     //Gestor de errores
     GLenum err;
     while ((err = glGetError()) != GL_NO_ERROR) {
@@ -284,7 +319,10 @@ int main()
     
     while (!glfwWindowShouldClose(window)) //glfwWindowShouldClose Checkea si window se quiere cerrar y mientras no se quiera cerrar da false.
     {
-
+        //calculo del delta time
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //Cambia el color de la ventana.
          //Limpia el buffer de profundudad para cambiarlo en el siguiente frame.
@@ -295,8 +333,8 @@ int main()
             //Creacion de las matrices
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        glm::mat4 view;
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         glm::mat4 projection = glm::mat4(1.0f);
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
@@ -323,7 +361,7 @@ int main()
         for (unsigned int i = 0; i < 10; i++)
         {
             glm::mat4 modelmatrix = glm::mat4(1.0f);
-            modelmatrix = glm::translate(modelmatrix, cubePositions[i]); //Usa glm translate para transladar la matriz usando el indice para usar los cubePosition definidos anteriormente
+            modelmatrix = glm::translate(modelmatrix, cubePositions[i]); //Usa glm transyoutulate para transladar la matriz usando el indice para usar los cubePosition definidos anteriormente
             float angle = 20.0f * i; //Multiplica el angulo por el indice para que cada cubo este en un angulo distinto
             modelmatrix = glm::rotate(modelmatrix, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f)); //Cambia el modelmatrix
             glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, glm::value_ptr(modelmatrix)); //Manda el modelo al uniform
@@ -346,28 +384,53 @@ int main()
 
 
 
-
-void checkOpenGLError(const char* stmt, const char* fname, int line) { //Comprueba errores comunes
-    GLenum err = glGetError();
-    while (err != GL_NO_ERROR) {
-        std::string error;
-        switch (err) {
-        case GL_INVALID_OPERATION:      error = "GL_INVALID_OPERATION";      break;
-        case GL_INVALID_ENUM:           error = "GL_INVALID_ENUM";           break;
-        case GL_INVALID_VALUE:          error = "GL_INVALID_VALUE";          break;
-        case GL_OUT_OF_MEMORY:          error = "GL_OUT_OF_MEMORY";          break;
-        case GL_INVALID_FRAMEBUFFER_OPERATION:  error = "GL_INVALID_FRAMEBUFFER_OPERATION";  break;
-        default:                        error = "Unknown error";             break;
-        }
-        std::cerr << "OpenGL error in file " << fname << " at line " << line << ": " << stmt << " - " << error << std::endl;
-        err = glGetError();
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse) //esto es para que cuando metas el raton en la ventana el raton este en el 000 y no en una esquina de la pantalla, ya que daria un salto
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
     }
+
+    float xoffset = xpos - lastX; //calculamos cuanto se ha movido en la x
+    float yoffset = lastY - ypos; //lo mismo en la y
+    lastX = xpos; //guardamos la posicion actual para usarla en el siguiente frame
+    lastY = ypos;
+
+    float sensitivity = 0.1f; //Esto se multiplica con el offset para manejar cuanto queremos que se mueva la camara segun el raton
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset; //sumamos la diferenia
+    pitch += yoffset;
+
+    if (pitch > 89.0f) //que no se pase de 90 grados o podrias darte la vuelta sobre ti mismo
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 direction; //Contenedor para calcular la direccion
+    //Ni idea de las mates que hace aqui pero pitch y yaw son los axis de las rotaciones
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction); //Lo normaliza para mandarlo hacia donde tiene que mirar la camara
 }
 
 void processInput(GLFWwindow* window) //Aqui proceso todos los inputs del teclado, raton etc (botones).
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) //Si se presiona el escape se cierra la ventana (glfwSetWindowShouldClose para window pasa a true).
         glfwSetWindowShouldClose(window, true);
+    float cameraSpeed = 2.5f * deltaTime; //velocidad
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) //Funcion para cambiar el Viewport al tamaño de la ventana.
 {
